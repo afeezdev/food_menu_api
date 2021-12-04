e898c0df25f3029b65878358a88e98ea4c008a57
afeezdev <afeezdev@gmail.com>
2021-12-04 13:59:50 +0100

    config updated

--- config/dbService.js
+++ config/dbService.js
@@ -2,13 +2,11 @@ const dotenv = require('dotenv');
 dotenv.config({ path: 'config/config.env'});
 const knex = require('knex');
 
-console.log(process.env.HOST)
-
 const db = knex({
     client: "mysql",
     connection: {
         host: process.env.HOST,
-        user: 'uwqkffxlhbawexpg',
+        user: process.env.USER_DB,
         password: process.env.PASSWORD,
         database: process.env.DATABASE,
     },
